07538aa516f7da13478306ac9c96efae1efc6a4e
bogdanstebelskiy <bogdanstebelskiy@gmail.com>
2025-04-15 18:55:51 +0300

    feat: setup next auth
    Setup NextAuth authentication and authorization with lib functions and client hooks.

---
+++ src/features/auth/config/auth.ts
@@ -0,0 +1,94 @@
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+
+import { getUserById } from '../lib/user'
+import { db } from '@/lib/db'
+import authConfig from './auth.config'
+import { getTwoFactorConfirmationByUserId } from '../lib/two-factor-confirmation'
+import { getAccountByUserId } from '../lib/account'
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  pages: {
+    signIn: '/auth/login',
+    error: '/auth/error',
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      })
+    },
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== 'credentials') return true
+
+      if (!user || !user.id) return false
+      const existingUser = await getUserById(user.id)
+
+      // Prevent sign in without email verification
+      if (!existingUser?.emailVerified) return false
+
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id,
+        )
+
+        if (!twoFactorConfirmation) return false
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        })
+      }
+
+      return true
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role
+      }
+
+      if (session.user) {
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.isOAuth = token.isOAuth
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+
+      const existingAccount = await getAccountByUserId(existingUser.id)
+
+      // Turn existingAccount to boolean
+      token.isOAuth = !!existingAccount
+      token.name = existingUser.name
+      token.email = existingUser.email
+      token.role = existingUser.role
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
+
+      return token
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: 'jwt' },
+  ...authConfig,
+})
