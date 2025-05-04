087e99c9f0dfee895be01871ed4bd3e9618be52e
bogdanstebelskiy <bogdanstebelskiy@gmail.com>
2025-05-04 18:08:35 +0300

    feat: create new verification page
    Created new verification page for auth.

---
+++ src/app/[locale]/auth/new-verification/page.tsx
@@ -0,0 +1,7 @@
+import { NewVerificationForm } from '@/features/auth/components/new-verification/new-verification-form'
+
+const NewVerificationPage = () => {
+  return <NewVerificationForm />
+}
+
+export default NewVerificationPage
