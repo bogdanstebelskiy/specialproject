f9371586fd93197edecf87d761f2fc66ed48d48c
bogdanstebelskiy <bogdanstebelskiy@gmail.com>
2025-05-04 18:09:09 +0300

    feat: create new password page
    Created new password page for auth.

---
+++ src/app/[locale]/auth/new-password/page.tsx
@@ -0,0 +1,7 @@
+import { NewPasswordForm } from '@/features/auth/components/new-password/new-password-form'
+
+const NewPasswordPage = () => {
+  return <NewPasswordForm />
+}
+
+export default NewPasswordPage
